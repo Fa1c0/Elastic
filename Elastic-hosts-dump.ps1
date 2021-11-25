87af7b96d7c5933f3f0e5069c64ab89ce14cdcaa
Fa1c0 <78224900+Fa1c0@users.noreply.github.com>
2021-11-25 13:59:43 +0000

    Add files via upload

---
+++ Elastic-hosts-dump.ps1
@@ -0,0 +1,90 @@
+﻿try { del .\Elastic_windows_servers.csv -ErrorAction SilentlyContinue}
+catch { }
+try { del .\Elastic_desktops.csv -ErrorAction SilentlyContinue}
+catch { }
+
+$User = Read-host "Enter Username"
+$Pass = Read-host "Enter Password"
+
+try{
+$servers = curl.exe -u $User':'$Pass -X GET -s "<elastic_search_endpoint>:9243/<windows_security_index>/_search" -H 'Content-Type: application/json' -d'
+{
+  ""query"": {
+    ""bool"": {
+      ""filter"": {
+        ""range"": {
+          ""@timestamp"": {
+            ""gte"": ""now-1h/h""
+          }
+        }
+      }
+    }
+  },
+  ""aggs"": {
+    ""hostname"": {
+      ""terms"": {
+        ""field"": ""agent.name"",
+        ""size"": 100
+      }
+    }
+  },""size"": 0
+}
+'
+}
+catch
+{
+write-host 'Servers failed.'
+}
+
+$servers = $servers -replace "}"
+$servers = $servers -replace '"'
+$servers = $servers.Split("{")
+$servers = $servers.Split(",") | findstr 'key'
+$servers = $servers -replace "key:"
+$servers >> Elastic_windows_servers.csv
+Write-Output ""
+Write-Output "$($servers.count) Windows Servers logging"
+
+try{
+$desktops = curl.exe -u $User':'$Pass -X GET -s "<elastic_search_endpoint>:9243/<desktops_security_index>/_search" -H 'Content-Type: application/json' -d'
+{
+  ""query"": {
+    ""bool"": {
+      ""filter"": {
+        ""range"": {
+          ""@timestamp"": {
+            ""gte"": ""now-1h/h""
+          }
+        }
+      }
+    }
+  },
+  ""aggs"": {
+    ""hostname"": {
+      ""terms"": {
+        ""field"": ""agent.name"",
+        ""size"": 100
+      }
+    }
+  },""size"": 0
+}
+'
+}
+catch
+{
+write-host 'Desktops failed.'
+}
+
+$desktops = $desktops -replace "}"
+$desktops = $desktops -replace '"'
+$desktops = $desktops.Split("{") 
+$desktops = $desktops.Split(",") | findstr 'key'
+$desktops = $desktops -replace "key:"
+$desktops >> Elastic_desktops.csv 
+
+Write-Output "$($desktops.count) Desktops logging"
+Write-Output ""
+Write-Output "Reports generated."
+Write-Output ""
+sleep 3
+Exit
